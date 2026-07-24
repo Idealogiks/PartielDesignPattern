0e7916ac44e86480163d16d4a3a25fa681eaee2f
idealogiks <bernard.glds@gmail.com>
2026-07-24 15:51:31 +0200

    feat: implementation méthode de base pour documents

--- documents/documents.js
+++ documents/documents.js
@@ -1,3 +1,36 @@
 export default class Documents {
+    constructor(item) {
+        this.item = item;
+    }
+
+    imprimerDocument(){
+        this.recupererInformations();
+        this.preparerEncre();
+        this.verifierPapier();
+        this.imprimer();
+        this.decouper();
+        this.operationSpecifique();
+        console.log("Fin de l'impression :emoji_pouce_en_l_air:\n");
+    }
+
+    recupererInformations(){
+        console.log(`Récupération des informations`);
+    }
+
+    preparerEncre(){
+        console.log("Préparation de l'encre");
+    }
+
+    verifierPapier(){
+        console.log("Vérification du papier");
+    }
+
+    imprimer(){
+        console.log("Impression");
+    }
+
+    decouper(){
+        console.log("Découpage"); //att je sais plus, faudra que ça, ça soit réadapté en fonction du format (sous-classes) je verrai plus tard pfpfpfpfpfpfpfp
+    }
     
 }
