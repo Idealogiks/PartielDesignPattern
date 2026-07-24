70be14a594ebc225f56b55cfa9f0c0e40ff6d295
idealogiks <bernard.glds@gmail.com>
2026-07-24 16:13:35 +0200

    feat: ajout système externe CPAM

---
+++ CPAM.js
@@ -0,0 +1,25 @@
+export default class CPAM {
+    fetchPatientData(){ 
+        console.log("CPAM - Récupération des informations "); 
+    }
+
+    initializeInk(){ 
+        console.log("CPAM - Préparation de l'encre"); 
+    }
+    
+    verifySupport(){ 
+        console.log("CPAM - Vérification du papier"); 
+    }
+
+    startPrint(){
+        console.log("CPAM - Impression"); 
+    }
+
+    cutCard(){
+        console.log("CPAM - Découpage"); 
+    }
+
+    finalizeCard(){ // Euhhhhhh ça remplace operationSpecifique????? jm pas
+        console.log("CPAM - Carte finie"); 
+    }
+}
